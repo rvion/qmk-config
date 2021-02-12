942f8b17151dbb17e5903a7c7a055fcda1b0ca69
rvion <vion.remi@gmail.com>
2021-02-12 19:51:41 +0100

    rename RAISE => MOVE

--- keymaps/rv1/keymap.c
+++ keymaps/rv1/keymap.c
@@ -12,7 +12,7 @@ enum layer_number {
     _COLEMAK,
     _LOWER,
     _NUMBERS,
-    _RAISE,
+    _MOVE,
     // _ADJUST
 };
 
@@ -28,7 +28,7 @@ enum custom_keycodes {
 };
 
 #define LOWER MO(_LOWER)
-#define RAISE MO(_RAISE)
+#define MOVE MO(_MOVE)
 #define NUMBERS MO(_NUMBERS)
 
 #define R_T3 MT(MOD_LCTL, KC_ENTER)
@@ -40,7 +40,7 @@ enum custom_keycodes {
 #define R_3 KC_3
 #define R_4 LT(_LOWER, KC_BSPACE)
 
-#define R_5 RAISE
+#define R_5 MOVE
 #define R_6 KC_6
 #define R_7 KC_7
 #define R_8 KC_8
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		LOWER,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
 		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                           KC_LEFT, KC_RGHT, R_T3,                          R_T4,    KC_UP,   KC_DOWN,
-                          KC_1,    KC_LWIN, KC_3,    R_4,         RAISE,   KC_6,    KC_7,    KC_8),
+                          KC_1,    KC_LWIN, KC_3,    R_4,         MOVE,   KC_6,    KC_7,    KC_8),
 
     [_COLEMAK] = KEYMAP(
      // _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______,
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		LOWER,   KC_A,    KC_R,    KC_S,    KC_T,    KC_D,        KC_H,    KC_N,    KC_E,    KC_I,    KC_O,     KC_GRV,
 		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_BSLS,
                           KC_LEFT, KC_RGHT, R_T3,                          R_T4,    KC_UP,   KC_DOWN,
-                          KC_1,    KC_LWIN, KC_3,    R_4,         RAISE,   KC_6,    KC_7,    KC_8),
+                          KC_1,    KC_LWIN, KC_3,    R_4,         MOVE,   KC_6,    KC_7,    KC_8),
 
 	[_LOWER]=KEYMAP(
 		_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,   KC_F7,   KC_F8,   KC_F9,   _______, RESET,
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		                  _______, _______, _______,                       _______, _______, _______,
 		                  KC_1,    KC_2,    KC_3,    KC_4,        KC_5,    KC_6,    COLEMAK, QWERTY),
 
-	[_RAISE]=KEYMAP(
+	[_MOVE]=KEYMAP(
 		_______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______,
 		_______, _______, _______, _______, _______, _______,     _______, KC_PGUP, KC_UP,   KC_PGDN, _______, _______,
 		_______, _______, _______, KC_LCTL, KC_LSFT, _______,     _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
