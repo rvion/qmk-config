089e9a5a49d8f93163e99197e63f11aab127ea85
rvion <vion.remi@gmail.com>
2021-02-12 21:14:41 +0100

    shift on bottom row

--- keymaps/rv1/keymap.c
+++ keymaps/rv1/keymap.c
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		LOWER,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
 		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                           KC_LEFT, KC_RGHT, R_T3,                          R_T4,    KC_UP,   KC_DOWN,
-                          KC_1,    KC_LWIN, KC_3,    R_4,         MOVE,    KC_6,    KC_7,    KC_8),
+                          KC_LWIN, KC_LSFT, KC_3,    R_4,         MOVE,    KC_6,    KC_7,    KC_8),
 
     [_COLEMAK] = KEYMAP(
      // _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______,
@@ -81,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		LOWER,   KC_A,    KC_R,    KC_S,    KC_T,    KC_D,        KC_H,    KC_N,    KC_E,    KC_I,    KC_O,     KC_GRV,
 		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_BSLS,
                           KC_LEFT, KC_RGHT, R_T3,                          R_T4,    KC_UP,   KC_DOWN,
-                          KC_1,    KC_LWIN, KC_3,    R_4,         MOVE,    KC_6,    KC_7,    KC_8),
+                          KC_LWIN, KC_LSFT, KC_3,    R_4,         MOVE,    KC_6,    KC_7,    KC_8),
 
 	[_LOWER]=KEYMAP(
 		_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,   KC_F7,   KC_F8,   KC_F9,   _______, RESET,
