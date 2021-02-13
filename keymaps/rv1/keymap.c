0915f69f6cae70ee8b0f1e59a3397d7e9288baf2
rvion <vion.remi@gmail.com>
2021-02-13 19:10:07 +0100

    use RV as global prefix for all custom keys

--- keymaps/rv1/keymap.c
+++ keymaps/rv1/keymap.c
@@ -29,37 +29,38 @@ enum custom_keycodes {
     QWERTY = SAFE_RANGE,
     COLEMAK,
 
-    A_E1,  // è
-    A_E2,  // é
-    A_E3,  // ê
-    A_E4   // ë
+    RV_E1,  // è
+    RV_E2,  // é
+    RV_E3,  // ê
+    RV_E4   // ë
+
 };
 
 #define LOWER MO(_LOWER)
 #define MOVE MO(_MOVE)
 #define NUMBERS MO(_NUMBERS)
 
-#define R_T3 MT(MOD_LCTL, KC_ENTER)
-#define R_T4 KC_SPC
+#define RV_T3 MT(MOD_LCTL, KC_ENTER)
+#define RV_T4 KC_SPC
 // MT(MOD_LSFT, KC_SPACE)
 
-#define R_1 KC_LWIN
-#define R_2 KC_LSFT
-#define R_3 KC_3
-#define R_4 LT(_LOWER, KC_BSPACE)
-#define R_5 MOVE
-#define R_6 KC_6
-#define R_7 KC_7
-#define R_8 KC_8
+#define RV_1 KC_LWIN
+#define RV_2 KC_LSFT
+#define RV_3 KC_3
+#define RV_4 LT(_LOWER, KC_BSPACE)
+#define RV_5 MOVE
+#define RV_6 KC_6
+#define RV_7 KC_7
+#define RV_8 KC_8
 
 // https://beta.docs.qmk.fm/using-qmk/simple-keycodes/keycodes_basic#punctuation
-#define R_PARO LSFT(KC_9)      // (
-#define R_PARC LSFT(KC_0)      // (
-#define R_BRAO LSFT(KC_RBRC)   // [
-#define R_BRAC LSFT(KC_LBRC)   // ]
-#define R_CURO KC_RBRC         // {
-#define R_CURC KC_LBRC         // }
-#define R_PLUS LSFT(KC_EQUAL)  // +
+#define RV_PARO LSFT(KC_9)      // (
+#define RV_PARC LSFT(KC_0)      // (
+#define RV_BRAO LSFT(KC_RBRC)   // [
+#define RV_BRAC LSFT(KC_LBRC)   // ]
+#define RV_CURO KC_RBRC         // {
+#define RV_CURC KC_LBRC         // }
+#define RV_PLUS LSFT(KC_EQUAL)  // +
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -70,8 +71,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
 		LOWER,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
 		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                          KC_LEFT, KC_RGHT, R_T3,                          R_T4,    KC_UP,   KC_DOWN,
-                          R_1,     R_2,     R_3,     R_4,         R_5,     R_6,     R_7,     R_8) ,
+                          KC_LEFT, KC_RGHT, RV_T3,                         RV_T4,   KC_UP,   KC_DOWN,
+                          RV_1,    RV_2,    RV_3,    RV_4,        RV_5,    RV_6,    RV_7,    RV_8),
 
     [_COLEMAK] = KEYMAP(
      // _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______,
@@ -79,20 +80,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,        KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,  KC_DEL,
 		LOWER,   KC_A,    KC_R,    KC_S,    KC_T,    KC_D,        KC_H,    KC_N,    KC_E,    KC_I,    KC_O,     KC_GRV,
 		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_BSLS,
-                          KC_LEFT, KC_RGHT, R_T3,                          R_T4,    KC_UP,   KC_DOWN,
-                          R_1,     R_2,     R_3,     R_4,         R_5,     R_6,     R_7,     R_8) ,
+                          KC_LEFT, KC_RGHT, RV_T3,                         RV_T4,   KC_UP,   KC_DOWN,
+                          RV_1,    RV_2,    RV_3,    RV_4,        RV_5,    RV_6,    RV_7,    RV_8),
 
 	[_LOWER]=KEYMAP(
 		_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,   KC_F7,   KC_F8,   KC_F9,   _______, RESET,
-		_______, _______, A_E1,    A_E2,    A_E3,    A_E4,        _______, R_PARC,  R_BRAO,  R_CURO,  _______, _______,
-		_______, _______, _______, _______, NUMBERS, _______,     KC_SLSH, R_PARO,  R_BRAC,  R_CURC,  _______, _______,
-		_______, _______, _______, _______, _______, _______,     R_PLUS,  KC_EQL,  KC_QUOT, KC_MINS, _______, _______,
+		_______, _______, RV_E1,   RV_E2,   RV_E3,   RV_E4,       _______, RV_PARC, RV_BRAO, RV_CURO, _______, _______,
+		_______, _______, _______, _______, NUMBERS, _______,     KC_SLSH, RV_PARO, RV_BRAC, RV_CURC, _______, _______,
+		_______, _______, _______, _______, _______, _______,     RV_PLUS, KC_EQL,  KC_QUOT, KC_MINS, _______, _______,
 		                  _______, _______, _______,                       KC_LSFT, _______, _______,
 		                  _______, _______, _______, _______,     _______, _______, COLEMAK, QWERTY),
 
 	[_NUMBERS]=KEYMAP(
 		_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,   KC_F7,   KC_F8,   KC_F9,   _______, RESET,
-		_______, _______, A_E1,    A_E2,    A_E3,    A_E4,        _______, KC_7,    KC_8,    KC_9,    _______, _______,
+		_______, _______, RV_E1,   RV_E2,   RV_E3,   RV_E4,       _______, KC_7,    KC_8,    KC_9,    _______, _______,
 		_______, _______, _______, _______, _______, _______,     _______, KC_4,    KC_5,    KC_6,    _______, _______,
 		_______, _______, _______, _______, _______, _______,     _______, KC_1,    KC_2,    KC_3,    _______, _______,
 		                  _______, _______, _______,                       _______, _______, _______,
@@ -124,16 +125,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case COLEMAK:
             if (record->event.pressed) { set_single_persistent_default_layer(_COLEMAK); }
             return false;
-        case A_E1:
+        case RV_E1:
             if (record->event.pressed) { SEND_STRING(SS_LALT(SS_TAP(X_KP_0) SS_TAP(X_KP_2) SS_TAP(X_KP_3) SS_TAP(X_KP_2))); }
             return false;
-        case A_E2:
+        case RV_E2:
             if (record->event.pressed) { SEND_STRING(SS_LALT(SS_TAP(X_KP_0) SS_TAP(X_KP_2) SS_TAP(X_KP_3) SS_TAP(X_KP_3))); }
             return false;
-        case A_E3:
+        case RV_E3:
             if (record->event.pressed) { SEND_STRING(SS_LALT(SS_TAP(X_KP_0) SS_TAP(X_KP_2) SS_TAP(X_KP_3) SS_TAP(X_KP_4))); }
             return false;
-        case A_E4:
+        case RV_E4:
             if (record->event.pressed) { SEND_STRING(SS_LALT(SS_TAP(X_KP_0) SS_TAP(X_KP_2) SS_TAP(X_KP_3) SS_TAP(X_KP_5))); }
             return false;
 
