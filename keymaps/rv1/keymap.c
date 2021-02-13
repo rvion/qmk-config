c858e26405b344709d501773b8d385453e3076fe
rvion <vion.remi@gmail.com>
2021-02-13 21:58:05 +0100

    remove led_set_user (I don't have leds)

--- keymaps/rv1/keymap.c
+++ keymaps/rv1/keymap.c
@@ -188,13 +188,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 // UNUSED
-void led_set_user(uint8_t usb_led) {
-    if (usb_led & (1 << USB_LED_NUM_LOCK)) { } else { }
-    if (usb_led & (1 << USB_LED_CAPS_LOCK)) { } else { }
-    if (usb_led & (1 << USB_LED_SCROLL_LOCK)) { } else { }
-    if (usb_led & (1 << USB_LED_COMPOSE)) { } else { }
-    if (usb_led & (1 << USB_LED_KANA)) { } else { }
-}
+// void led_set_user(uint8_t usb_led) {
+//     if (usb_led & (1 << USB_LED_NUM_LOCK)) { } else { }
+//     if (usb_led & (1 << USB_LED_CAPS_LOCK)) { } else { }
+//     if (usb_led & (1 << USB_LED_SCROLL_LOCK)) { } else { }
+//     if (usb_led & (1 << USB_LED_COMPOSE)) { } else { }
+//     if (usb_led & (1 << USB_LED_KANA)) { } else { }
+// }
 
 
 // https://beta.docs.qmk.fm/using-qmk/software-features/feature_combo
