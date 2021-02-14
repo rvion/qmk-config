9e19898d69b3ef2f920ccaff22cb3b290e52c96b
rvion <vion.remi@gmail.com>
2021-02-14 06:18:23 +0100

    make IPv4 easier to type

--- keymaps/rv1/keymap.c
+++ keymaps/rv1/keymap.c
@@ -107,9 +107,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_EMOJIS] = KEYMAP(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,   KC_F7,   KC_F8,   KC_F9,   _______, RESET,
-        _______, _______, KC_PGUP, KC_UP,   KC_PGDN, _______,     X(HND1), KC_7,    KC_8,    KC_9,    X(GREN), _______,
-        _______, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,      X(HND2), KC_4,    KC_5,    KC_6,    X(ORNG), _______,
-        _______, _______, KC_TAB,  _______, _______, _______,     X(HND3), KC_1,    KC_2,    KC_3,    X(RED),  _______,
+        _______, _______, KC_PGUP, KC_UP,   KC_PGDN, _______,     X(HND1), KC_7,    KC_8,    KC_9,    X(GREN), X(ORNG),
+        _______, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,      X(HND2), KC_4,    KC_5,    KC_6,    X(RED),  _______,
+        _______, _______, KC_TAB,  _______, _______, _______,     X(HND3), KC_1,    KC_2,    KC_3,    KC_DOT,  _______,
                           _______, _______, _______,                       _______, _______, _______,
                           _______, _______, _______, KC_LALT,     _______, _______, _______, _______),
 
