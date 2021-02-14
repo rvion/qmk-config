9d280fe7227fbd09140ae5a6e1153e99a6220291
rvion <vion.remi@gmail.com>
2021-02-14 06:05:19 +0100

    easier '?'

--- keymaps/rv1/keymap.c
+++ keymaps/rv1/keymap.c
@@ -75,7 +75,7 @@ enum custom_keycodes {
 #define RV_CURC KC_LBRC         // }
 #define RV_PLUS LSFT(KC_EQUAL)  // +
 #define RV_ACPT LCTL(KC_ENTER)  // accept(ctrl+enter)
-
+#define RV_QMRK LSFT(KC_SLSH)
 // #define RV_BANG LSFT(KC_1)   // !
 
 // clang-format off
@@ -100,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_SYMBOLS] = KEYMAP(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,   KC_F7,   KC_F8,   KC_F9,   _______, RESET,
         _______, _______, RV_E1,   RV_E2,   RV_E3,   RV_E4,       _______, RV_PARC, RV_BRAO, RV_CURO, _______, _______,
-        _______, RV_A1,   RV_A2,   RV_ACPT, KC_APP,  _______,     KC_SLSH, RV_PARO, RV_BRAC, RV_CURC, _______, _______,
+        _______, RV_A1,   RV_A2,   RV_ACPT, RV_QMRK, KC_APP,      KC_SLSH, RV_PARO, RV_BRAC, RV_CURC, _______, _______,
         _______, _______, _______, RV_C1,   _______, _______,     RV_PLUS, KC_EQL,  KC_QUOT, KC_MINS, _______, _______,
                           _______, _______, _______,                       _______, _______, _______,
                           _______, _______, _______, _______,     _______, KC_LSFT, COLEMAK, QWERTY),
